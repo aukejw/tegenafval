a4607018ace2cfc49c952921bea8eba84dfd7416
Auke Wiggers <wiggers.auke@gmail.com>
2025-07-11 10:15:56 +0200

    Fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const base = '/tegenafval/';
+
 // https://astro.build/config
 export default defineConfig({
     redirects: {
-        '/': '/nl/home',
-        '/probleem': '/nl/probleem',
-        '/oplossingen': '/nl/oplossingen',
-        '/gemeente': '/nl/gemeente'
+        '/': base + 'nl/home',
+        '/probleem': base + 'nl/probleem',
+        '/oplossingen': base + 'nl/oplossingen',
+        '/gemeente': base + 'nl/gemeente'
     },
 
     output: 'static',
 
-    base: '/tegenafval/'
+    base: base
 });
